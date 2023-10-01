07ab794fbe0e3470d486c22211256c338e5a63aa
zaraazzar <azzar.arza18@gmail.com>
2023-10-02 06:04:44 +0700

    change updatePeriodMillis

--- app/src/main/java/com/kitapacak/wecan/widget/RiwayatDonasiWidget.kt
+++ app/src/main/java/com/kitapacak/wecan/widget/RiwayatDonasiWidget.kt
@@ -1,9 +1,14 @@
 package com.kitapacak.wecan.widget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.widget.RemoteViews
+import com.kitapacak.wecan.DonasiOtomatisActivity
+import com.kitapacak.wecan.IsiSaldoActivity
 import com.kitapacak.wecan.R
 
 /**
@@ -29,6 +34,18 @@ class RiwayatDonasiWidget : AppWidgetProvider() {
 internal fun updateAppWidget2(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.riwayat_donasi_widget)
+
+    val buildCheck = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE else 0
+
+    //Buat Intent ke Isi Saldo Activity
+    val intentToIsiSaldo = Intent(context, IsiSaldoActivity::class.java)
+    val pendingIntent = PendingIntent.getActivity(context,0,intentToIsiSaldo,buildCheck)
+    views.setOnClickPendingIntent(R.id.btn_isi2,pendingIntent)
+
+    //Intent ke Activity Donasi Otomatis
+    val intentToDO = Intent(context, DonasiOtomatisActivity::class.java)
+    val pendingIntent2 = PendingIntent.getActivity(context,0,intentToDO,buildCheck)
+    views.setOnClickPendingIntent(R.id.donasiOtomatis,pendingIntent2)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
